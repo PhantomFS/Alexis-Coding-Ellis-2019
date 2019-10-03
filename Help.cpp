1af9dfef6605b7ea5ff27ceeb46e02382972f1fc
Alex47 <alexishstarr@gmail.com>
2019-10-03 21:40:59 +0000

    y

---
+++ Help.cpp
@@ -0,0 +1,25 @@
+// 010-TestCase.cpp
+
+// Let Catch provide main():
+#define NAME "Player"
+
+#include <stdio.h>
+
+char speak(char b){
+
+    char* myString=NAME;
+    char a;
+    a='0';
+
+
+    printf("Hello %s, Type a number \n",myString);
+    scanf("%c",&a);
+    printf("%s, %c was typed \n",myString,a);
+}
+
+int main(){
+speak(1);
+
+
+
+}
